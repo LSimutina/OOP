67ad4e51a2579c6e5f979ff5678885832c8938fe
Lidiya <simutina.1994@mail.ru>
2024-07-27 20:36:44 +0300

    added task 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+//        6. ООП
+//        Задание 4
+//        Задание 3
+//        Задание 2
+
+//        Задание 1
+
+        Point coordinate_1 = new Point(1, 3);
+        Point coordinate_2 = new Point(1, 3);
+        Point coordinate_3 = new Point(5, 8);
+
+        System.out.println(coordinate_1);
+        System.out.println(coordinate_2);
+        System.out.println(coordinate_3);
+
+        System.out.println(coordinate_1 == coordinate_2);
+        System.out.println(coordinate_2 == coordinate_3);
+
+    }
 }
