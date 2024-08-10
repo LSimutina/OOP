91325889431367e9a1572cbe8ce4df1b60d01090
Lidiya <simutina.1994@mail.ru>
2024-08-10 21:42:48 +0300

    Исправлено задание 4 Сотрудники и отделы

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -11,9 +11,25 @@ public class Employee {
         this.dep = dep;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDep() {
+        return dep;
+    }
+
+    public void setDep(Department dep) {
+        this.dep = dep;
+    }
+
     @Override
     public String toString() {
-        if (this == dep.boss) return name + " начальник отдела " + dep.name;
+        if (this == dep.getBoss()) return name + " начальник отдела " + dep.getName();
         return name + " работает в отделе " + dep;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,29 @@ public class Main {
 //        6. ООП
 //        Задание 4. Сотрудник
 
-        Employee boss = new Employee("Anton");
-        Department dep = new Department("IT", boss);
+        Employee bossIt = new Employee("Anton");
+        Department depIt = new Department("IT", bossIt);
+        bossIt.setDep(depIt);
+        Department depMarket = new Department("Market");
 
-        Employee emp1 = new Employee("Petr", dep);
-        Employee emp2 = new Employee("Anna", dep);
+        Employee empIt = new Employee("Petr", depIt);
+        Employee emp2It = new Employee("Anna", depIt);
+        Employee empMarket = new Employee("Vanya", depMarket);
+        Employee emp2Market = new Employee("Liza", depMarket);
+        depMarket.setBoss(emp2Market);
 
-        System.out.println(emp1);
-        System.out.println(emp2);
+        System.out.println(empIt);
+        System.out.println(emp2It);
+        System.out.println(empMarket);
+        System.out.println(emp2Market);
 
-        dep.boss = emp1;
-        System.out.println(emp1);
-        System.out.println(emp2);
+        System.out.println(" ");
 
-
+        depIt.setBoss(empMarket);
+        System.out.println(empIt);
+        System.out.println(emp2It);
+        System.out.println(empMarket);
+        System.out.println(emp2Market);
 
 //        Задание 3. Студенты
 /*
